eee9d982c555e4e23545950d189c1f0c489c83e1
RiteshPx <parmaritesh17@gmail.com>
2025-08-04 13:58:11 +0530

    check connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,9 @@ require("dotenv").config();
 const dbconnect=()=>{
     mongoose.connect(process.env.DATABASE_URL).then(()=>{
         console.log("Connected with the database successfully.") }).catch((error)=>{
-            "Error in connecting with the database."
+            "Error in connecting with the database.:", error.message;
+        process.exit(1);                    
+        // Exit the process with failure    
         })
 }
 
---
+++ backend/utils/MailSender.js
@@ -0,0 +1,30 @@
+const nodemailer = require('nodemailer');
+const emailTemplate = require('./emailTemplate');
+require('dotenv').config();
+
+const MailSender= async(email,title,otp)=>{
+    try{
+        let transporter = nodemailer.createTransport({
+            host :process.env.MAIL_HOST,
+            auth: {
+                user: process.env.MAIL_USER, 
+                pass: process.env.MAIL_PASS, 
+            },
+        });
+
+        let info =await transporter.sendMail({
+            from: "testing the project -Ritesh Parmar",
+            to:`${email}`,
+            subject:`${title}`,
+            html: emailTemplate(otp),
+        })
+        console.log(info);
+        return info;
+
+    }
+    catch(error){
+        console.log(error.message);
+    }
+}
+
+module.exports = MailSender;
